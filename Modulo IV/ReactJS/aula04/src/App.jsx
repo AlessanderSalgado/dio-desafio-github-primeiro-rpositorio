a5c50c11c322338a88d40179268e20872eb10506
AlessanderSalgado <99378398+AlessanderSalgado@users.noreply.github.com>
2022-05-08 00:40:19 -0300

    Organizando as pastas

---
+++ Modulo IV/ReactJS/aula04/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+const App = () => {
+    return(
+        <div>
+            <p>Digital Inovation</p>
+            <p>Bem Vindo a nossa Aula</p>
+            <div>Webpack 4</div>
+            <div><Button class="button"></Button></div>
+        </div>
+    );
+};
+export default App;
