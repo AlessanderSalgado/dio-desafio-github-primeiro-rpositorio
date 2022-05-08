8efbb6efd24fa1828a6c70f735d2964bfc0eb203
AlessanderSalgado <99378398+AlessanderSalgado@users.noreply.github.com>
2022-05-08 02:52:54 -0300

    Atualização aula04/fim aula05/inicio

--- Modulo IV/ReactJS/aula04/src/App.jsx
+++ Modulo IV/ReactJS/aula04/src/App.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import Button from './Button';
+
+const buttonA = <button>Historico de clientes</button>
+const buttonB = <button>Cadastrar Cliente</button>
+
+const hasCustomer = true
 const App = () => {
+    const renderShowHistory = (
+        <div>
+            Clice no botao para visualizar o Historico:<br />
+            {buttonA}
+        </div>
+    )
+    const renderAddCustomer = (
+        <div>
+            Clique para cadastrar o cliente: <br />
+            {buttonB}
+        </div>
+    )
+    
+        const customer = 'Alessander Salgado'
+
+        const showCustomer = () => {
+            if (!hasCustomer) return null
+            return (
+                <div>
+                    <h1>Cliente:  Alessander Salgado</h1>
+                </div>
+            )
+        }
+
     return(
         <div>
             <p>Digital Inovation</p>
             <p>Bem Vindo a nossa Aula</p>
-            <div>Webpack 4</div>
-            <div><Button class="button"></Button></div>
+            {hasCustomer ? renderShowHistory : renderAddCustomer}
+            {showCustomer()}
         </div>
     );
 };
