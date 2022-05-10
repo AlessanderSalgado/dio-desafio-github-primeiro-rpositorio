68d25484ef1fe600dcc94bbbbeebdb3f226e052c
AlessanderSalgado <99378398+AlessanderSalgado@users.noreply.github.com>
2022-05-10 16:23:57 -0300

    Atualização aula05 Chaves

--- Modulo IV/ReactJS/aula05/src/App.jsx
+++ Modulo IV/ReactJS/aula05/src/App.jsx
@@ -3,26 +3,42 @@ import React from 'react';
 const listCustomer = [
     {
         id: 1,
-        name: 'Alessander'
+        name: 'Alessander',
+        skills: ['react', 'Node', 'CSS', 'Webpack']
     },
     {
         id: 2,
-        name: 'Marcelo'
+        name: 'Marcelo',
+        skills: ['HTML', 'React Native',, 'Go', 'Js']
     },
     {
         id: 3,
-        name: 'Patricia'
+        name: 'Patricia',
+        skills: ['Java']
     },
     {
         id: 4,
-        name: 'Caio'
+        name: 'Caio',
+        skills: ['Peasson']
     }
 ]
 
 const App = () => {
     
     const renderCustomers = (customer, index) => {
-        return (<li key={index}>{customer.name}</li>)
+        return (
+        <div key={'customer-${cutomer.id}'}>           
+            <li>{customer.name}</li>
+            {customer.skills.map(renderSkills)}
+        </div>
+        )
+    }
+    const renderSkills = (skill, index) => {
+        return (
+            <div style={{paddingLeft: '30px'}}>
+                <li key={'skill-${index}'}>{skill}</li>
+            </div>
+        )
     }
     return(
         <div>
